1c3a515f227b43ae33dd415c3b6f358ec11f5e3b
pothibo <pothibo@gmail.com>
2017-01-12 09:38:05 -0500

    Only load middleware if env is not a test one

--- lib/firebase/railtie.rb
+++ lib/firebase/railtie.rb
@@ -5,13 +5,15 @@ module Firebase
   class Railtie < Rails::Railtie
     config.firebase = Firebase::Configuration.new
 
-    initializer 'firebase.middleware' do |app|
-      app.middleware.use Firebase::Middleware
-    end
+    unless Rails.env.test?
+      initializer 'firebase.middleware' do |app|
+        app.middleware.use Firebase::Middleware
+      end
 
-    config.before_initialize do
-      if config.firebase.base_url.blank?
-        raise "You need to set a base URL for firebase. config.firebase.base_url = 'https://<your-firebase>.firebaseio.com/'"
+      config.before_initialize do
+        if config.firebase.base_url.blank?
+          raise "You need to set a base URL for firebase. config.firebase.base_url = 'https://<your-firebase>.firebaseio.com/'"
+        end
       end
     end
   end
